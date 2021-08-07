e698f46b8cea603b410e4b0e613b77d59046b438
Rafael Ramos <rafaelsramos95@gmail.com>
2021-08-07 22:24:23 +0100

    create setStatusBarDrawable method

--- app/src/main/java/com/android/utils/Utils.kt
+++ app/src/main/java/com/android/utils/Utils.kt
@@ -1,3 +1,23 @@
 package com.android.utils
 
+import android.app.Activity
+import android.graphics.Color
+import android.view.Window
+import android.view.WindowManager
+import androidx.annotation.DrawableRes
+import androidx.core.content.ContextCompat
+
 const val DATE_FORMAT_PATTERN = "yyyy-MM-dd HH:mm:ss"
+
+/**
+ * Set a gradient to the activity's [Window] background, allowing for a smooth
+ * status bar + action bar transition
+ * @param activity Reference to an [Activity]
+ */
+fun setStatusBarDrawable(activity: Activity, @DrawableRes drawableRes: Int ) {
+    with (activity.window) {
+        addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        statusBarColor = Color.TRANSPARENT
+        setBackgroundDrawable(ContextCompat.getDrawable(activity, drawableRes))
+    }
+}
